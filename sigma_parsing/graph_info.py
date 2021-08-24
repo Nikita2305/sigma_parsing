08dbae108d668c7573a94fa1d9a982a39ee6ac86
Nikita2305 <nikitanikitin2305@gmail.com>
2021-08-24 09:12:58 +0300

    Построен граф

--- sigma_parsing/graph_info.py
+++ sigma_parsing/graph_info.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
+from pathlib import Path
 
 def getstr(var):
     if (isinstance(var, str)):
--- sigma_parsing/visualizer.py
+++ sigma_parsing/visualizer.py
@@ -7,57 +7,77 @@ def getstr(var):
         return var
     return ""
 
-def is_official(members, user_id):
+def get_status(our_id, official_id):
+    if (our_id == -1):
+        return "not selected but search successfull"
+    if (our_id == official_id):
+        return "selected and matched"
+    return "selected but not match"
+    
+def find_by_id(members, s_id):
     for member in members:
-        if (member["processed"] and member["official_page"]["id"] == user_id):
-            return True
-    return False
+        for account in member["vk_pages"]:
+            if (account["id"] == s_id):
+                return account
+    return {}
 
-with open("output/members.txt") as f:
-    users = json.load(f)
+files = [path for path in Path('./output').rglob('members04*.txt')]
+if (len(files) >= 1):
+    print("Type a number:")
+    for i in range(len(files)):
+        print(i, ":", files[i])
+    i = int(input())
+    with open(files[i % len(files)]) as f:
+        users = json.load(f) 
+else:
+    print("No file")
+    quit()
+
+
+ids = []
+for user in users:
+    if (user["processed"]):
+        ids += [(user["official_page"]["id"], user["vk_id"], user)]
+    else:
+        if (user["vk_id"] in [account["id"] for account in user["vk_pages"]]):
+            ids += [(-1, user["vk_id"], user)]
 
 wb = openpyxl.Workbook()
 ws = wb.active
-ws.title = "Edges"
+ws.title = "Vertices"
+ws["A1"] = "Id"
+ws["B1"] = "Label"
+ws["C1"] = "Status"
+ws["D1"] = "School"
+usr = 2
+final_ids = []
+for pair in ids:
+    if (pair[0] == -1): 
+        account = find_by_id(users, pair[1])
+    else:
+        account = find_by_id(users, pair[0])
+    id1 = account["id"]
+    if (id1 in final_ids):
+        continue
+    final_ids += [id1]
+    ws["A"+str(usr)] = str(id1)
+    ws["B"+str(usr)] = account["first_name"] + " " + account["last_name"]
+    ws["C"+str(usr)] = get_status(pair[0], pair[1])
+    ws["D"+str(usr)] = pair[2]["school"]
+    usr += 1
+
+ws = wb.create_sheet("Edges")
 ws["A1"] = "Source"
 ws["B1"] = "Target"
 ws["C1"] = "Type"
 edge = 2
-ids = []
-for user in users:
-    if (not user["processed"]):
-        continue
-    account = user["official_page"]
-    id1 = account["id"]
-    if (id1 in ids):
-        continue
-    ids += [id1]
+for id1 in final_ids:
+    account = find_by_id(users, id1)
     for id2 in account["friends"]:
-        if (not is_official(users,id2)):
-            continue
-        ws["A"+str(edge)] = str(id1)
-        ws["B"+str(edge)] = str(id2)
-        ws["C"+str(edge)] = "Undirected"
+        if (id2 in final_ids):
+            ws["A"+str(edge)] = str(id1)
+            ws["B"+str(edge)] = str(id2)
+            ws["C"+str(edge)] = "Undirected"
         edge += 1
 
-ws = wb.create_sheet("Vertices")
-ws["A1"] = "Id"
-ws["B1"] = "Label"
-ws["C1"] = "Olymps"
-ws["D1"] = "School"
-usr = 2
-ids = []
-for user in users:
-    if (not user["processed"]):
-        continue
-    account = user["official_page"]
-    id1 = account["id"]
-    if (id1 in ids):
-        continue
-    ids += [id1]
-    ws["A"+str(usr)] = str(id1)
-    ws["B"+str(usr)] = getstr(user["name"]) + " " + getstr(user["surname"])
-    ws["C"+str(usr)] = str([getstr(olymp["olymp"]) + "(" + getstr(olymp["class"]) + " класс)" for olymp in user["diplomas"]])
-    ws["D"+str(usr)] = getstr(user["school"])
-    usr += 1
 wb.save("output/graph.xlsx")
