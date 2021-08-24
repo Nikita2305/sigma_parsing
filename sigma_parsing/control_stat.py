fff15611cc4a3f21b7554fdbebdcc9814ec3257d
Anton Shumakov <shuum117@gmail.com>
2021-08-24 11:26:35 +0600

    descision trees

--- sigma_parsing/control_stat.py
+++ sigma_parsing/control_stat.py
@@ -1,6 +1,9 @@
 import json
 import sys
 import functools
+from sklearn import tree
+import numpy as np
+from sklearn.tree import export_text
 
 def getstr(var):
     if (isinstance(var, str)):
@@ -55,3 +58,45 @@ print("not_selected:",not_selected)
 print("not_selected_but_search_successful:",not_selected_but_search_successful)
 
 print("m2>0 ",len([l for l in lst if (l[0][1] if len(l[0]) > 1 else 0) > 0]))
+
+def to_features(member):
+    feat = []
+    feat.append(len(member["vk_pages"]))
+    l = [(len(account["friends"]), not account["is_closed"] or account["can_access_closed"]) for account in member["vk_pages"]]
+    l.sort(reverse=True)
+    feat.append(l[0][0] if len(l) > 0 else 0)
+    feat.append(l[1][0] if len(l) > 1 else 0)
+    feat.append(1 if len(l) > 0 and l[0][1] else 0)
+    feat.append(1 if len(l) > 1 and l[1][1] else 0)
+    return feat
+    
+def to_class(member):
+    vk_id = member["vk_id"]
+    pages = member["vk_pages"]
+    if(len(pages) >= 2 and pages[1]["id"] == vk_id):
+        return "M2-case"
+    elif(len(pages) >= 1 and pages[0]["id"] == vk_id):
+        return "M1-case"
+    else:
+        return "Undefined"
+
+x = [to_features(member) for member in members]
+y = [to_class(member) for member in members]
+
+clf = tree.DecisionTreeClassifier(max_depth=6)
+clf = clf.fit(x, y)
+
+r = export_text(clf, feature_names=["n","m1","m2","open1","open2"])
+print(r)
+
+if len(sys.argv) >= 3:
+    filename2 = sys.argv[2]
+    with open(filename2) as f:
+        members2 = json.load(f)
+    for mem in members2:
+        if not mem["processed"]:
+            continue
+        res = clf.predict_proba([to_features(mem)])
+        l = [(len(account["friends"]), not account["is_closed"] or account["can_access_closed"],account["id"]) for account in mem["vk_pages"]]
+        l.sort(reverse=True, key = lambda tup : (tup[0],tup[1]))
+        print(res," ",l[0][2] if len(l) > 0 else ""," ",l[1][2] if len(l) > 1 else ""," processed:",mem["official_page"]["id"] if mem["processed"] else "NO")
--- sigma_parsing/plot_drawer.py
+++ sigma_parsing/plot_drawer.py
@@ -1,8 +1,6 @@
 import json
 import sys
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 import numpy as np
 
 def getstr(var):
@@ -18,51 +16,21 @@ if len(sys.argv) >= 2:
 with open(filename) as f:
     members = json.load(f)
 
-active = {}
-for i in range(-1,1000):
-    active[i] = 0
-
 lst = []
 for member in members:
-    if (not "vk_id" in member):
+    if (len(member["vk_pages"]) == 0):
         continue
-    if (len(member["official_page"]) == 0):
-        if(len(member["vk_pages"]) != 0):
-            for acc in member["vk_pages"]:
-                if(acc["id"] == int(member["vk_id"])):
-                    if (len(member["vk_pages"]) == 0):
-                        active[-1] = active[-1] + 1
-                        continue
-                    l = [len(account["friends"]) for account in member["vk_pages"]]
-                    l.sort()
-                    l = l[::-1]
-                    lst += [(l, getstr(member["surname"]) + " " + getstr(member["name"]))]
-                    break
-lst.sort()
+    l = [len(account["friends"]) for account in member["vk_pages"]]
+    l.sort()
+    l = l[::-1]
+    lst += [(l, getstr(member["surname"]) + " " + getstr(member["name"]))]
 
-for l in lst:
-    active[l[0][0]] = active[l[0][0]] + 1 
-
-keys = list(active.keys())[:50]
-values = list(active.values())[:50]
-plt.bar(keys, values)
-plt.ylabel("Number of people")
-plt.xlabel("M1")
-plt.savefig("output/M1.png")
-plt.clf()
-
-keys = list(active.keys())[:50]
-values = list(active.values())[:50]
-for i in reversed(range(1, len(keys))):
-    values[i - 1] += values[i]
-plt.bar(keys, values)
-plt.ylabel("Number of people(cumulative)")
-plt.xlabel("M1")
-plt.savefig("output/M1_cumulative.png")
-plt.clf()
+print(len(members))
+print(len(lst))
 
 X = np.array([l[0][0] for l in lst])
 Y = np.array([(l[0][1] if len(l[0]) > 1 else 0) for l in lst])
+Z = np.array([len(l[0]) for l in lst])
 
 fig, ax = plt.subplots()
 hist, xbins, ybins, im = ax.hist2d(X, Y, bins=(30,30), range=[(0,30),(0,30)], cmin=1)
@@ -74,5 +42,26 @@ for i in range(len(ybins)-1):
 
 
 plt.savefig("output/M1_M2.png")
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+hist, xbins, im = ax.hist(X, bins=30, range=(0,30))
+
+plt.savefig("output/M1.png")
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+hist, xbins, im = ax.hist(Y, bins=30, range=(0,30))
+
+plt.savefig("output/M2.png")
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+hist, xbins, im = ax.hist(Z, bins=10, range=(0,10))
+
+plt.savefig("output/N.png")
 plt.show()
 plt.clf()
